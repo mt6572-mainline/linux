f2f368c6028efe9d5aa54fcda28774996a7ebd2e
Dinolek <git@dinolek.me>
2025-07-06 20:19:12 +0200

    drm/mediatek: dsi: Clear DSI_INTSTA before requesting irq
    This is needed for cases where bootloader didn't clear it, which leads to
    handler being stuck.
    
    Signed-off-by: Dinolek <git@dinolek.me>

--- drivers/gpu/drm/mediatek/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mtk_dsi.c
@@ -1246,6 +1246,7 @@ static int mtk_dsi_probe(struct platform_device *pdev)
 	if (ret < 0)
 		return dev_err_probe(dev, ret, "Failed to register DSI host\n");
 
+	writel(0, dsi->regs + DSI_INTSTA);
 	ret = devm_request_irq(&pdev->dev, irq_num, mtk_dsi_irq,
 			       IRQF_TRIGGER_NONE, dev_name(&pdev->dev), dsi);
 	if (ret) {

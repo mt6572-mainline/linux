4af9976408a58b476446290c3ed5e9bc8c3cdee3
Yassine Oudjana <y.oudjana@protonmail.com>
2025-12-18 01:57:37 +0000

    drm/mediatek: dsi: Do not restore mode on sucessful host transfer
    The DSI controller on MT6735 needs some time between successive command-video-command
    mode switches, and therefore currently fails to switch back to command mode after
    a host transfer followed by a video mode switch. These successive mode switches mainly
    happen in mtk_dsi_host_transfer, where the mode is restored after performing a transfer,
    resulting in mode switches on each transfer in a sequence (panel initialization, for
    example), with the time between a switch back to video mode and a switch to command mode
    for a new transfer being separated by just a few microseconds. To avoid this, leave the
    DSI controller in command mode after a successful host transfer since it is very likely
    that there will be more transfers upcoming after one. A call to mtk_dsi_output_enable
    sets the appropriate mode for panel operation, so restoring the mode on host transfers
    is not strictly necessary.
    
    Mode switches should not be necessary for host transfers since the DSI controller
    supports sending commands while in video mode, but that is not implemented at the
    moment, so this is the only way to roll for now.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

--- drivers/gpu/drm/mediatek/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mtk_dsi.c
@@ -1146,10 +1146,8 @@ static ssize_t mtk_dsi_host_transfer(struct mipi_dsi_host *host,
 	if (ret)
 		goto restore_dsi_mode;
 
-	if (!MTK_DSI_HOST_IS_READ(msg->type)) {
-		recv_cnt = 0;
-		goto restore_dsi_mode;
-	}
+	if (!MTK_DSI_HOST_IS_READ(msg->type))
+		return 0;
 
 	if (!msg->rx_buf) {
 		DRM_ERROR("dsi receive buffer size may be NULL\n");
@@ -1179,13 +1177,15 @@ static ssize_t mtk_dsi_host_transfer(struct mipi_dsi_host *host,
 	DRM_INFO("dsi get %zd byte data from the panel address(0x%x)\n",
 		 recv_cnt, *((u8 *)(msg->tx_buf)));
 
+	return recv_cnt;
+
 restore_dsi_mode:
 	if (dsi_mode & MODE) {
 		mtk_dsi_set_mode(dsi);
 		mtk_dsi_start(dsi);
 	}
 
-	return ret < 0 ? ret : recv_cnt;
+	return ret;
 }
 
 static const struct mipi_dsi_host_ops mtk_dsi_ops = {
